e4ef2f5c5eaab566582545d424544115af6e0f3b
Daniel Dias Barbosa <danieldiasbarbo@gmail.com>
2024-10-07 09:52:31 -0300

    Added log to boleto generation

--- charge/worker.py
+++ charge/worker.py
@@ -4,8 +4,13 @@ from .dtos import DebtDTO
 class Worker:
     processed_debts_db = set()
 
+    def _generate_boleto(self, debt: DebtDTO):
+        boleto = f"Favor {debt.name}, pague o montante R${debt.debt_amount}"
+        print(f"Boleto para {debt.debt_id} gerado")
+        return boleto
+
     # Should be declared in a separated module with the SMTP connection and error handlings
-    def send_email(self, email, content):
+    def _send_email(self, email, content):
         print(f"Mail sent to {email}, content: {content}")
 
     def _check_if_debt_is_already_processed(self, debt: DebtDTO):
@@ -16,9 +21,7 @@ class Worker:
         for debt in debts_dtos:
             if not self._check_if_debt_is_already_processed(debt):
                 self.processed_debts_db.add(debt.debt_id)
-                self.send_email(
-                    debt.email,
-                    f"Favor {debt.name}, pague o montante R${debt.debt_amount}",
-                )
+                boleto = self._generate_boleto(debt)
+                self._send_email(debt.email, boleto)
             else:
                 print(f"Débito {debt.debt_id} já processado")
